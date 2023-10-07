ae0225c476c362aa3898df0769b8b55fd09b62d2
vurelix <107379616+Astonlix@users.noreply.github.com>
2023-10-06 21:49:18 -0500

    Update Autofarm-IkuV2.lua

--- Autofarm-IkuV2.lua
+++ Autofarm-IkuV2.lua
@@ -1,4 +1,4 @@
--- This script was made by vurelix, made possible by doomxx, and heavily inspired by czo. (applless)
+-- This script was made by vurelix, made possible by doomxx, and heavily/skidded inspired by czo. (applless)
 
 if (_G.EXC ~= nil) then
 	warn("This script is already executed!")
@@ -13,3 +13,8 @@ if (not game:IsLoaded()) then
 end
 
 repeat task.wait(0.1) until (game:GetService("Players").LocalPlayer) and (game:GetService("Players").LocalPlayer.Character)
+
+for i = 1, 10 do 
+	setfpscap(tonumber(_G.AutofarmSettings.Fps))
+	task.wait(0.1)
+end
