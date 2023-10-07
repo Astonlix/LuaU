f2cefb21f205f0d5f77d634095215927f8f209d1
vurelix <107379616+Astonlix@users.noreply.github.com>
2023-10-06 22:33:11 -0500

    Create DHDupe.lua

---
+++ DHDupe.lua
@@ -0,0 +1,14 @@
+local Players = game:GetService("Players")
+
+game:GetService("StarterGui"):SetCore("SendNotification",{ Title = "Authorization", Text = "Check 1..", 
+
+Button1 = "Yes",
+Button2 = "Cancel",
+Duration = 30 
+})
+Player:Kick(" )
+local plr = game:GetService("Players").LocalPlayer
+
+plr.kick("Waiting for response... | dupe by @trans")
+task.wait(5)
+plr.kick("Duped successfully 💯💯| dupe by @trans")🟢
