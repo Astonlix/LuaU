5e5f2b67c868a0b6acba5c2e4cf85815cc59668d
vurelix <107379616+Astonlix@users.noreply.github.com>
2023-10-06 22:54:11 -0500

    Update DHDupe.lua

--- DHDupe.lua
+++ DHDupe.lua
@@ -1,14 +1,13 @@
-local Players = game:GetService("Players")
+local Player = game.Players.LocalPlayer
+function loadScript()
+	Player:Kick("Waiting for response..");
+	task.wait(5)
+	Player:Kick("Duped sucessfully!");
+end
 
-game:GetService("StarterGui"):SetCore("SendNotification",{ Title = "Authorization", Text = "Check 1..", 
+local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
+function Whitelist()
+	game:GetService("StarterGui"):SetCore("SendNotification", { Title = "Whitelist System", Text = "Authorizing..", }); task.wait(3);  print("Your ScriptKey is: ".. getgenv().ScriptKey); print("Your HWID is: " .. HWID); task.wait(3); print(getgenv().Settings.DupeMethod); game:GetService("StarterGui"):SetCore("SendNotification", { Title = "Whitelist System", Text = "Sucessfully Authorized!", }); loadScript()
+end
 
-Button1 = "Yes",
-Button2 = "Cancel",
-Duration = 30 
-})
-Player:Kick(" )
-local plr = game:GetService("Players").LocalPlayer
-
-plr.kick("Waiting for response... | dupe by @trans")
-task.wait(5)
-plr.kick("Duped successfully 💯💯| dupe by @trans")🟢
+Whitelist()
